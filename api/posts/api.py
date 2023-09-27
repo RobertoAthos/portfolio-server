9dff6646d3ac9eaeb9fef7010419075ef4131bcb
Roberto Athos <robertoathos@bricker.vc>
2023-09-27 09:42:12 -0300

    fix: serializer now it´s possible

--- api/posts/api.py
+++ api/posts/api.py
@@ -1,32 +1,46 @@
 from django.shortcuts import get_object_or_404
 from ninja import NinjaAPI
-from .schemas import PostSchemaIn, NotFoundSchema,PostSchemaOut
-from .models import Posts
 from typing import List
+from datetime import date
 
+from .schemas import PostSchemaIn, NotFoundSchema, PostSchemaOut, UpdatePostSchema
+from .models import Posts
 
 api = NinjaAPI()
 
+
 @api.get("/test")
 def test(request):
-    return {"message":"aoabaaa"}
+    return {"message": "aoabaaa"}
+
 
 @api.post("/create-post")
 def create_post(request, payload: PostSchemaIn):
+    payload.date = date.today()
     post = Posts.objects.create(**payload.dict())
-    return {"post": post}
+    post_data = {
+        "id": post.id,
+        "title": post.title,
+        "subtitle": post.subtitle,
+        "content": post.content,
+        "date": post.date.strftime("%Y-%m-%d") if post.date else None,
+    }
+    return {"post": post_data}
+
 
 @api.get("/post/{post_id}", response=PostSchemaOut)
 def get_post(request, post_id: int):
     post = get_object_or_404(Posts, id=post_id)
     return post
 
+
 @api.get("/posts", response=List[PostSchemaOut])
 def posts(request):
     posts = Posts.objects.all()
     return posts
 
-@api.patch("post/{post_id}")
+
+@api.put("/update_post/{post_id}")
 def update_post(request, post_id: int, payload: PostSchemaIn):
     post = get_object_or_404(Posts, id=post_id)
     for attr, value in payload.dict().items():
@@ -34,8 +48,9 @@ def update_post(request, post_id: int, payload: PostSchemaIn):
     post.save()
     return {"success": "post updated successfully"}
 
-@api.delete("/post/{post_id}")
-def delete_post(request, post_id:int):
+
+@api.delete("/delete_post/{post_id}")
+def delete_post(request, post_id: int):
     post = get_object_or_404(Posts, id=post_id)
     post.delete()
-    return {"success": "post updated successfully"}
+    return {"success": "post deleted successfully"}
--- api/posts/schemas.py
+++ api/posts/schemas.py
@@ -1,12 +1,12 @@
 from ninja import Schema
 from datetime import date
-
+from typing import Optional
 
 class PostSchemaIn(Schema):
     title: str
     subtitle: str
     content: str
-    date: date = None
+    date: Optional[date] = None
 
 class PostSchemaOut(Schema):
     title: str
@@ -14,5 +14,11 @@ class PostSchemaOut(Schema):
     content: str
     date: date
 
+class UpdatePostSchema(Schema):
+    title: Optional[str]
+    subtitle: Optional[str]
+    content: Optional[str]
+    date: Optional[date]
+    
 class NotFoundSchema(Schema):
     message: str
