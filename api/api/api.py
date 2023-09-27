84b98ce5e60e61f78d5df1af9a6a9b2b62969df9
Roberto Athos <robertoathos@bricker.vc>
2023-09-27 11:02:03 -0300

    feat(chat): started a chatbot app

--- api/api/api.py
+++ api/api/api.py
@@ -1,7 +1,9 @@
 from ninja import NinjaAPI
 from posts.api import router as event_post
+from chat.api import router as event_chat
 
 api = NinjaAPI()
 
 
 api.add_router("/posts", event_post)
+api.add_router("/chat", event_chat)
